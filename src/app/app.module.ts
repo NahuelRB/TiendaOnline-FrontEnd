dcb9941b6b6ff9cceb4bbedbbe0a804da20277cb
morbdeath <nahuel_barbosa1990@hotmail.com.ar>
2021-06-30 22:19:35 -0300

    commit trabajando en el footer contactenos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PersonasComponent } from './personas/personas.component';
+import { PersonaComponent } from './persona/persona.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactenosComponent } from './contactenos/contactenos.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonasComponent,
+    PersonaComponent,
+    HeaderComponent,
+    ContactenosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule//Es para utilizar two-binding
   ],
   providers: [],
   bootstrap: [AppComponent]
