f659939fd74d5e5aff5149dca5bb506bee84b50e
morbdeath <nahuel_barbosa1990@hotmail.com.ar>
2021-07-13 13:36:33 -0300

    Separados los componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { Order } from './Entidades/Order';
 
 @Component({
-  selector: 'app-holamundo',
+  selector: 'app-tienda',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'miPrimeraApp';
+
+
+  name: string = ""
+  lastname: string = ""
+  phoneNumber: string = ""
+  email: string = ""
+
+  inputName: string = ""
+  inputLastname: string = ""
+  inputPhonenumber: string = ""
+  inputEmail: string = ""
+
+  orders: Order[] = [new Order('nahuel', 'barbosa', '1123234343', 'probando@gmail.com')];
+
+
+  guardarDatos(event: Event) {
+    this.name = (<HTMLInputElement>event.target).value;
+    this.lastname = (<HTMLInputElement>event.target).value;
+    this.phoneNumber = (<HTMLInputElement>event.target).value;
+    this.email = (<HTMLInputElement>event.target).value;
+  }
+
+  orderAdd(order: Order) {
+    this.orders.push(order);//PUSH: guardar order en mi lista array orders
+  }
+
+  /*addClient(){
+      let pedidos2 = new pedido(this.inputName,this.inputLastname,this.inputPhonenumber,this.inputEmail);
+      this.pedidos.push(pedidos2);
+  }*/
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PersonasComponent } from './personas/personas.component';
-import { PersonaComponent } from './persona/persona.component';
-import { HeaderComponent } from './header/header.component';
-import { ContactenosComponent } from './contactenos/contactenos.component';
+import { HeaderComponent } from './View/header/header.component';
+import { ContactenosComponent } from './View/contactenos/contactenos.component';
 import { FormsModule } from '@angular/forms';
+import { ListarPersonasComponent } from './View/listar-personas/listar-personas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PersonasComponent,
-    PersonaComponent,
     HeaderComponent,
-    ContactenosComponent
+    ContactenosComponent,
+    ListarPersonasComponent
   ],
   imports: [
     BrowserModule,
